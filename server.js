d929e95ef91622f75f3954d74767140770ea89a9
fioportu <portuguezfiore@gmail.com>
2025-07-19 12:28:34 -0600

    Conexión a Mongo / Página Principal

---
+++ server.js
@@ -0,0 +1,35 @@
+// server.js
+// node server.js
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
+require('dotenv').config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Conexión a MongoDB
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('✅ Conectado a MongoDB'))
+.catch(err => console.error('❌ Error de conexión:', err));
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Mostrar index.html
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Iniciar servidor
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
+});
