b765c1f3737f018fc2903ad6ed6aa864cc7f3ca2
samorafi <sebasmora172522@gmail.com>
2025-08-18 17:44:39 -0600

    Ajuste tareas

--- models/tarea.model.js
+++ models/tarea.model.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose');
 
 const tareaSchema = new mongoose.Schema({
-  _id: { type: String }, // IDs personalizados como "t001", "t002"
+  //_id: { type: String }, // IDs personalizados como "t001", "t002"
+  _id: {
+    type: String,
+    default: () => new mongoose.Types.ObjectId().toString()
+  },
   titulo: { type: String, required: true },
   descripcion: { type: String },
   id_proyecto: { type: String, required: true }, // referencia a proyecto
